4e133a353fb758bbe1ad60f8d274aace17fddbf3
DELL <DELL@DESKTOP-D6BEUQT>
2022-06-20 16:31:33 +0530

    new updated classes

---
+++ src/test/java/Practice/ByeTest.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class ByeTest {
+	
+	@Test
+	public void simple()
+	{
+		System.out.println("have a nice day");
+		
+	}
+
+}
---
+++ src/test/java/Practice/HiiTest.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class HiiTest {
+	@Test
+	public void dispu()
+	{
+		System.out.println("its hii test");
+	}
+
+}
