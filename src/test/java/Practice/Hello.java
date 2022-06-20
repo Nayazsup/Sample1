39feb92750d0e91b9a41334b88b697554bd5dd48
DELL <DELL@DESKTOP-D6BEUQT>
2022-06-20 15:23:31 +0530

    sample code commited

---
+++ src/test/java/Practice/Hello.java
@@ -0,0 +1,19 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Hello {
+	@Test
+	public void disp()
+	{
+		System.out.println("i am java");
+		
+		System.out.println("i am mava");
+
+		System.out.println("i am nava");
+
+		System.out.println("i am oava");
+
+	}
+
+}
